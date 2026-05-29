17315a747bb449ad08eec13e3d7c3406ee2ba277
MihailKashinsev <mihailkasincevv@gail.com>
2026-05-29 03:03:22 +0300

    Keep web push label visible

--- lib/app_version.dart
+++ lib/app_version.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/foundation.dart';
+
 /// Версия для отображения в UI. При релизе обновляй вместе с [pubspec.yaml] `version: X.Y.Z+N`.
 class AppVersion {
   AppVersion._();
@@ -8,6 +10,8 @@ class AppVersion {
       String.fromEnvironment('RLINK_WEB_PUSH_ID', defaultValue: '');
 
   static String get label => '$version ($buildNumber)';
-  static String get webPushLabel =>
-      webPushId.isEmpty ? '' : 'Web push $webPushId';
+  static String get webPushLabel {
+    if (!kIsWeb) return '';
+    return 'Web push ${webPushId.isEmpty ? 'unknown' : webPushId}';
+  }
 }
